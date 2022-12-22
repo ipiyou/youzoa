550f969df8d4262cda9f5d944dd90a709fe1fb59
ipiyou <tao710803@gmail.com>
2022-12-22 14:16:16 +0900

    font exo 추가

--- src/style/global.ts
+++ src/style/global.ts
@@ -1,6 +1,7 @@
 import { css } from "@emotion/react";
 
 const global = css`
+  @import url("https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,400;1,100&display=swap");
   * {
     margin: 0;
     padding: 0;

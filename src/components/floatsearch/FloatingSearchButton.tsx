3765c2b8afde94fc355b0fa9478e8dfd64809762
ipiyou <tao710803@gmail.com>
2022-12-30 13:58:24 +0900

    디자인 개선

--- src/components/floatsearch/FloatingSearchButton.tsx
+++ src/components/floatsearch/FloatingSearchButton.tsx
@@ -29,13 +29,13 @@ const _Wrapper = styled.div<{ x: number; y: number }>`
   z-index: 100;
   display: flex;
   border-radius: 100px;
+  background-color: ${({ theme }) => theme.draw.setting};
   transform: translate3d(${({ x, y }) => `${x}px,${y}px,0`});
 `;
 
 const GainWidth_Keyframes = keyframes`
     0%{
         width: 0px;
-        border: 1px solid black;
     }
     100%{
         width: 300px;
@@ -51,7 +51,6 @@ const RemoveInput = keyframes`
     }
     100%{
         width: 0px;
-        border: 0;
         padding: 0;
     }
 `;
@@ -60,9 +59,10 @@ const _InputKeyWord = styled.input<{ animation: boolean | null }>`
   width: 0px;
   border: 0;
   outline: 0;
-  border-radius: 50px 0 0 50px;
   font-size: 16px;
-  background-color: #d9d9d9;
+  border-radius: 50px 0 0 50px;
+  color: ${({ theme }) => theme.font.fontcolor};
+  background-color: transparent;
   animation: ${({ animation }) =>
     typeof animation !== "object" &&
     css`1s ${animation ? GainWidth_Keyframes : RemoveInput} forwards`};
@@ -73,6 +73,8 @@ const _SearchIcon = styled(SearchIcon)`
   width: 50px;
   height: 50px;
   padding: 10px;
+  border-radius: 50px;
+  background-color: ${({ theme }) => theme.draw.selector};
   path {
     fill: ${({ theme }) => theme.draw.background};
   }

b7080ae3588347dc97f30127547a8bf01e0e639c
ipiyou <tao710803@gmail.com>
2023-02-08 00:17:25 +0900

    키워드 검색시 데이터 캐싱

--- src/hooks/useCallVideo.ts
+++ src/hooks/useCallVideo.ts
@@ -1,28 +1,34 @@
 import { getVideoList, ReSponse, YouTube } from "../apis/GetVideoList";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import useResultMap from "./useResultMap";
 
-const useCallVideo = () => {
-  let { data, isLoading, fetchNextPage } = useInfiniteQuery(
+const useCallVideo = (searchingWord = "default") => {
+  const { Map, MapPush, currentMap } = useResultMap();
+  let { isFetchingNextPage, fetchNextPage } = useInfiniteQuery(
     ["querykey"],
-    ({ pageParam }) => getVideoList(pageParam),
+    ({ pageParam }) => getVideoList(searchingWord, pageParam),
     {
+      onSuccess: (data) => {
+        const LastIndex = data.pages.length - 1;
+        MapPush(searchingWord, data.pages[LastIndex].items);
+      },
       getNextPageParam: (lastPage) => lastPage.nextPageToken,
+      staleTime: Infinity,
+      cacheTime: 5000,
     }
   );
 
-  const divideYoutube = (data: ReSponse[] | undefined) => {
-    if (!data) return;
-    const items: YouTube[] = [];
-    data.forEach((e) => items.push(...e.items));
-
+  const divideYoutube = () => {
+    const items: YouTube[] = [...currentMap(searchingWord)];
     const LastElement = items.pop();
 
     return { items, LastElement };
   };
+  console.log(Map);
 
   return {
-    YouTube: divideYoutube(data?.pages),
-    isLoading,
+    YouTube: divideYoutube(),
+    isLoading: isFetchingNextPage,
     CallNextYoutube: fetchNextPage,
   };
 };
---
+++ src/hooks/useResultMap.ts
@@ -0,0 +1,36 @@
+import { YouTube } from "../apis/GetVideoList";
+import { useState } from "react";
+
+interface MapType {
+  Map: {
+    name: string;
+    result: YouTube[];
+  }[];
+  nameMap: string[];
+}
+
+const useResultMap = () => {
+  const [Maps, setMap] = useState<MapType>({
+    Map: [],
+    nameMap: [],
+  });
+  const { Map, nameMap } = Maps;
+  const keywordSearch = (keyword: string) => nameMap.includes(keyword);
+  const mapIndex = (keyword: string) => nameMap.indexOf(keyword);
+
+  const MapPush = (keyword: string, arg: YouTube[]) => {
+    const currentItem = Map[mapIndex(keyword)];
+    if (keywordSearch(keyword)) currentItem.result.push(...arg);
+    else
+      setMap({
+        Map: [...Map, { name: keyword, result: [...arg] }],
+        nameMap: [...nameMap, keyword],
+      });
+  };
+
+  const currentMap = (keyword: string) =>
+    keywordSearch(keyword) ? Map[mapIndex(keyword)].result : [];
+  return { Map, MapPush, currentMap };
+};
+
+export default useResultMap;
